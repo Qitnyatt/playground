aa33e73e321159317b742ec6eb95a0beafb114e2
Qitnyatt <Qitnyatt@protonmail.com>
2019-12-16 20:20:34 +0000

    Added codewars.com:basic-mathematical-operations

---
+++ codewars.com/basic-mathematical-operations.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/basic-mathematical-operations/train/javascript
+
+
+function basicOp(operation, value1, value2)
+{
+    return eval(`${value1}${operation}${value2}`);
+}
