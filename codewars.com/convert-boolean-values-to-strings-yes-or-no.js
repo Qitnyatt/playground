194f3d1a0ce8177ce8c61a0a608165f6cf2d3700
Qitnyatt <Qitnyatt@protonmail.com>
2019-12-16 20:17:10 +0000

    Added codewars.com:convert-boolean-values-to-strings-yes-or-no

---
+++ codewars.com/convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no/train/javascript
+
+
+function boolToWord(bool) {
+    if (bool) {
+        return 'Yes';
+    }
+    return 'No';
+}
