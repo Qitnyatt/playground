1eb26f50d3de348194ca00a5ae3b72bf7b7d6d72
Qitnyatt <Qitnyatt@protonmail.com>
2019-12-16 20:07:48 +0000

    Added codewars.com:find-the-smallest-integer-in-the-array

---
+++ codewars.com/find-the-smallest-integer-in-the-array.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args);
+    }
+}
