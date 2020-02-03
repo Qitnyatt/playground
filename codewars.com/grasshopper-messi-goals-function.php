b936c35e33545b41ca46956dd0900d93f65ca4a1
Qitnyatt <Qitnyatt@protonmail.com>
2020-02-03 15:58:38 +0000

    Added codewars.com:grasshopper-messi-goals-function

---
+++ codewars.com/grasshopper-messi-goals-function.php
@@ -0,0 +1,8 @@
+<?php
+
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/php
+
+function goals(int $laLigaGoals, int $copaDelReyGoals, int $championsLeagueGoals): int
+{
+    return $laLigaGoals + $copaDelReyGoals + $championsLeagueGoals;
+}
