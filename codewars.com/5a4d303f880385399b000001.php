1ac4d51e45f314e46c5ba7f6f87762bfac4e0aff
Qitnyatt <Qitnyatt@protonmail.com>
2020-02-04 22:01:36 +0000

    Added codewars.com:5a4d303f880385399b000001

---
+++ codewars.com/5a4d303f880385399b000001.php
@@ -0,0 +1,21 @@
+<?php
+
+// https://www.codewars.com/kata/5a4d303f880385399b000001/train/php/**/
+
+function strong($n)
+{
+    $factorial = function ($n) use (&$factorial) {
+        return $n <= 1 ? 1 : $n * $factorial($n - 1);
+    };
+    $current = 0;
+    foreach (array_map('intval', str_split(strval($n))) as $number) {
+        $current += $factorial($number);
+        if ($current > $n) {
+            return "Not Strong !!";
+        }
+    }
+    if ($current == $n) {
+        return "STRONG!!!!";
+    }
+    return "Not Strong !!";
+}
